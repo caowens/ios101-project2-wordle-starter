ef20e99334ed0f7606c9b1fd662e59eff63ff4e7
caowens <caowens555@yahoo.com>
2023-09-25 14:11:51 -0500

    Exercise 2: Change number of guesses

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -76,7 +76,9 @@ class BoardController: NSObject,
   // Checkpoint: Correctly implementing this should allow you to change the number of rows in the board!
   private func applyNumGuessesSettings(with settings: [String: Any]) {
     // START YOUR CODE HERE
-    // ...
+      if let numGuesses = settings[kNumGuessesKey] as? Int {
+          numRows = numGuesses
+      }
     // END YOUR CODE HERE
   }
   
